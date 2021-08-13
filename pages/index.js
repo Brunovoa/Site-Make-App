ad9f92439e147ec93147bd819734a2893c499edc
Bruno Alves <74555040+Brunovoa@users.noreply.github.com>
2021-08-13 19:53:49 -0300

    teste 000003

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Link from 'next/Link'
 
 function Home() {
-    return ( <div>é isso ai
-        <Link href="/lar">
+    return ( <div>
+        <h1>é isso ai</h1>
+        <Link href="/sobre">
         <a>testando</a>
         </Link>
         </div>
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/Link'
+
+function sobre() {
+    return ( <div>
+        <h1>é isso ala</h1>
+        <Link href="/">
+        <a>testando</a>
+        </Link>
+        </div>
+        )
+}
+
+export default sobre
